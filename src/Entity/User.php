2a587095127dead5d7d93a8ddf8aecda62731f8a
issam liqali <63265720+travolta11@users.noreply.github.com>
2023-07-07 22:06:00 +0100

    ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Entity;
+
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -14,11 +15,9 @@ use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 
-
 #[ApiResource]
 #[ApiFilter(SearchFilter::class, strategy: 'word_start')]
 #[ApiFilter(OrderFilter::class, properties: ['created_at' => 'DESC'])]
-
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -33,22 +32,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private array $roles = [];
 
-   
-
-   /**
- * @var string|null The hashed password
- */
-#[ORM\Column]
-#[Assert\NotBlank]
-#[Assert\Length(
-    min: 5,
-    max: 20,
-    minMessage: 'Your password must be at least {{ limit }} characters long',
-    maxMessage: 'Your password name cannot be longer than {{ limit }} characters',
-)]
-private ?string $password = null;
-
-
+    /**
+     * @var string|null The hashed password
+     */
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 5,
+        max: 20,
+        minMessage: 'Your password must be at least {{ limit }} characters long',
+        maxMessage: 'Your password name cannot be longer than {{ limit }} characters',
+    )]
+    private ?string $password = null;
 
     #[ORM\Column(length: 100)]
     private ?string $nom = null;
@@ -89,9 +84,6 @@ private ?string $password = null;
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Vital::class)]
     private Collection $vitals;
 
-    
-    
-    
     public function __construct()
     {
         $this->roles = $roles ?? ["medecin", "reception", "admin"];
@@ -105,8 +97,6 @@ private ?string $password = null;
         $this->vitals = new ArrayCollection();
     }
 
-
-
     public function getId(): ?int
     {
         return $this->id;
@@ -117,28 +107,22 @@ private ?string $password = null;
         return $this->email;
     }
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-
-
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
+    // Update the getEmail() method to getUserIdentifier()
     public function getUserIdentifier(): string
     {
         return (string) $this->email;
     }
 
-    /**
-     * @see UserInterface
-     */
+    // Remove the setEmail() method
+    // Remove the setUsername() method
+
+    // Update the setEmail() method to setUserIdentifier()
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
     public function getRoles(): array
     {
         $roles = $this->roles;
@@ -156,7 +140,17 @@ private ?string $password = null;
     }
 
     /**
-     * @see PasswordAuthenticatedUserInterface
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
      */
     public function getPassword(): string
     {
@@ -173,17 +167,26 @@ private ?string $password = null;
     /**
      * @see UserInterface
      */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
     public function getNom(): ?string
     {
         return $this->nom;
     }
 
-    public function setNom(string $nom): self
+    public function setNom(?string $nom): self
     {
         $this->nom = $nom;
 
@@ -195,273 +198,9 @@ private ?string $password = null;
         return $this->adresse;
     }
 
-    public function setAdresse(string $adresse): self
+    public function setAdresse(?string $adresse): self
     {
         $this->adresse = $adresse;
-
-        return $this;
-    }
-
-    public function getTel(): ?string
-    {
-        return $this->tel;
-    }
-
-    public function setTel(string $tel): self
-    {
-        $this->tel = $tel;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Rendezvous>
-     */
-    public function getRendezvouses(): Collection
-    {
-        return $this->rendezvouses;
-    }
-
-    public function addRendezvouse(Rendezvous $rendezvouse): self
-    {
-        if (!$this->rendezvouses->contains($rendezvouse)) {
-            $this->rendezvouses->add($rendezvouse);
-            $rendezvouse->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeRendezvouse(Rendezvous $rendezvouse): self
-    {
-        if ($this->rendezvouses->removeElement($rendezvouse)) {
-            // set the owning side to null (unless already changed)
-            if ($rendezvouse->getUser() === $this) {
-                $rendezvouse->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Patient>
-     */
-    public function getPatients(): Collection
-    {
-        return $this->patients;
-    }
-
-    public function addPatient(Patient $patient): self
-    {
-        if (!$this->patients->contains($patient)) {
-            $this->patients->add($patient);
-            $patient->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removePatient(Patient $patient): self
-    {
-        if ($this->patients->removeElement($patient)) {
-            // set the owning side to null (unless already changed)
-            if ($patient->getUser() === $this) {
-                $patient->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Ordonance>
-     */
-    public function getOrdonances(): Collection
-    {
-        return $this->ordonances;
-    }
-
-    public function addOrdonance(Ordonance $ordonance): self
-    {
-        if (!$this->ordonances->contains($ordonance)) {
-            $this->ordonances->add($ordonance);
-            $ordonance->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeOrdonance(Ordonance $ordonance): self
-    {
-        if ($this->ordonances->removeElement($ordonance)) {
-            // set the owning side to null (unless already changed)
-            if ($ordonance->getUser() === $this) {
-                $ordonance->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Medicament>
-     */
-    public function getMedicaments(): Collection
-    {
-        return $this->medicaments;
-    }
-
-    public function addMedicament(Medicament $medicament): self
-    {
-        if (!$this->medicaments->contains($medicament)) {
-            $this->medicaments->add($medicament);
-            $medicament->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMedicament(Medicament $medicament): self
-    {
-        if ($this->medicaments->removeElement($medicament)) {
-            // set the owning side to null (unless already changed)
-            if ($medicament->getUser() === $this) {
-                $medicament->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Categoriem>
-     */
-    public function getCategoriems(): Collection
-    {
-        return $this->categoriems;
-    }
-
-    public function addCategoriem(Categoriem $categoriem): self
-    {
-        if (!$this->categoriems->contains($categoriem)) {
-            $this->categoriems->add($categoriem);
-            $categoriem->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCategoriem(Categoriem $categoriem): self
-    {
-        if ($this->categoriems->removeElement($categoriem)) {
-            // set the owning side to null (unless already changed)
-            if ($categoriem->getUser() === $this) {
-                $categoriem->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Examentest>
-     */
-    public function getExamentests(): Collection
-    {
-        return $this->examentests;
-    }
-
-    public function addExamentest(Examentest $examentest): self
-    {
-        if (!$this->examentests->contains($examentest)) {
-            $this->examentests->add($examentest);
-            $examentest->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeExamentest(Examentest $examentest): self
-    {
-        if ($this->examentests->removeElement($examentest)) {
-            // set the owning side to null (unless already changed)
-            if ($examentest->getUser() === $this) {
-                $examentest->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Examenresultat>
-     */
-    public function getExamenresultats(): Collection
-    {
-        return $this->examenresultats;
-    }
-
-    public function addExamenresultat(Examenresultat $examenresultat): self
-    {
-        if (!$this->examenresultats->contains($examenresultat)) {
-            $this->examenresultats->add($examenresultat);
-            $examenresultat->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeExamenresultat(Examenresultat $examenresultat): self
-    {
-        if ($this->examenresultats->removeElement($examenresultat)) {
-            // set the owning side to null (unless already changed)
-            if ($examenresultat->getUser() === $this) {
-                $examenresultat->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Vital>
-     */
-    public function getVitals(): Collection
-    {
-        return $this->vitals;
-    }
-
-    public function addVital(Vital $vital): self
-    {
-        if (!$this->vitals->contains($vital)) {
-            $this->vitals->add($vital);
-            $vital->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeVital(Vital $vital): self
-    {
-        if ($this->vitals->removeElement($vital)) {
-            // set the owning side to null (unless already changed)
-            if ($vital->getUser() === $this) {
-                $vital->setUser(null);
-            }
-        }
 
         return $this;
     }
@@ -478,7 +217,275 @@ private ?string $password = null;
         return $this;
     }
 
+    public function getTel(): ?string
+    {
+        return $this->tel;
+    }
+
+    public function setTel(?string $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Rendezvous[]
+     */
+    public function getRendezvouses(): Collection
+    {
+        return $this->rendezvouses;
+    }
+
+    public function addRendezvous(Rendezvous $rendezvous): self
+    {
+        if (!$this->rendezvouses->contains($rendezvous)) {
+            $this->rendezvouses[] = $rendezvous;
+            $rendezvous->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRendezvous(Rendezvous $rendezvous): self
+    {
+        if ($this->rendezvouses->contains($rendezvous)) {
+            $this->rendezvouses->removeElement($rendezvous);
+            // set the owning side to null (unless already changed)
+            if ($rendezvous->getUser() === $this) {
+                $rendezvous->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Patient[]
+     */
+    public function getPatients(): Collection
+    {
+        return $this->patients;
+    }
+
+    public function addPatient(Patient $patient): self
+    {
+        if (!$this->patients->contains($patient)) {
+            $this->patients[] = $patient;
+            $patient->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removePatient(Patient $patient): self
+    {
+        if ($this->patients->contains($patient)) {
+            $this->patients->removeElement($patient);
+            // set the owning side to null (unless already changed)
+            if ($patient->getUser() === $this) {
+                $patient->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Ordonance[]
+     */
+    public function getOrdonances(): Collection
+    {
+        return $this->ordonances;
+    }
+
+    public function addOrdonance(Ordonance $ordonance): self
+    {
+        if (!$this->ordonances->contains($ordonance)) {
+            $this->ordonances[] = $ordonance;
+            $ordonance->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrdonance(Ordonance $ordonance): self
+    {
+        if ($this->ordonances->contains($ordonance)) {
+            $this->ordonances->removeElement($ordonance);
+            // set the owning side to null (unless already changed)
+            if ($ordonance->getUser() === $this) {
+                $ordonance->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Medicament[]
+     */
+    public function getMedicaments(): Collection
+    {
+        return $this->medicaments;
+    }
+
+    public function addMedicament(Medicament $medicament): self
+    {
+        if (!$this->medicaments->contains($medicament)) {
+            $this->medicaments[] = $medicament;
+            $medicament->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMedicament(Medicament $medicament): self
+    {
+        if ($this->medicaments->contains($medicament)) {
+            $this->medicaments->removeElement($medicament);
+            // set the owning side to null (unless already changed)
+            if ($medicament->getUser() === $this) {
+                $medicament->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Categoriem[]
+     */
+    public function getCategoriems(): Collection
+    {
+        return $this->categoriems;
+    }
+
+    public function addCategoriem(Categoriem $categoriem): self
+    {
+        if (!$this->categoriems->contains($categoriem)) {
+            $this->categoriems[] = $categoriem;
+            $categoriem->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategoriem(Categoriem $categoriem): self
+    {
+        if ($this->categoriems->contains($categoriem)) {
+            $this->categoriems->removeElement($categoriem);
+            // set the owning side to null (unless already changed)
+            if ($categoriem->getUser() === $this) {
+                $categoriem->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Examentest[]
+     */
+    public function getExamentests(): Collection
+    {
+        return $this->examentests;
+    }
+
+    public function addExamentest(Examentest $examentest): self
+    {
+        if (!$this->examentests->contains($examentest)) {
+            $this->examentests[] = $examentest;
+            $examentest->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExamentest(Examentest $examentest): self
+    {
+        if ($this->examentests->contains($examentest)) {
+            $this->examentests->removeElement($examentest);
+            // set the owning side to null (unless already changed)
+            if ($examentest->getUser() === $this) {
+                $examentest->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Examenresultat[]
+     */
+    public function getExamenresultats(): Collection
+    {
+        return $this->examenresultats;
+    }
+
+    public function addExamenresultat(Examenresultat $examenresultat): self
+    {
+        if (!$this->examenresultats->contains($examenresultat)) {
+            $this->examenresultats[] = $examenresultat;
+            $examenresultat->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExamenresultat(Examenresultat $examenresultat): self
+    {
+        if ($this->examenresultats->contains($examenresultat)) {
+            $this->examenresultats->removeElement($examenresultat);
+            // set the owning side to null (unless already changed)
+            if ($examenresultat->getUser() === $this) {
+                $examenresultat->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Vital[]
+     */
+    public function getVitals(): Collection
+    {
+        return $this->vitals;
+    }
+
+    public function addVital(Vital $vital): self
+    {
+        if (!$this->vitals->contains($vital)) {
+            $this->vitals[] = $vital;
+            $vital->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVital(Vital $vital): self
+    {
+        if ($this->vitals->contains($vital)) {
+            $this->vitals->removeElement($vital);
+            // set the owning side to null (unless already changed)
+            if ($vital->getUser() === $this) {
+                $vital->setUser(null);
+            }
+        }
+
+        return $this;
+    }
 }
-
-
-
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+// namespace App\Tests\Entity;
+
+// use App\Entity\User;
+// use PHPUnit\Framework\TestCase;
+
+// class UserTest extends TestCase
+// {
+//     public function testUserEntity()
+//     {
+//         $user = new User();
+
+//         $user->setEmail('test@example.com');
+//         $user->setRoles(['ROLE_USER']);
+//         $user->setPassword('password123');
+//         $user->setNom('John Doe');
+//         $user->setAdresse('123 Street');
+//         $user->setTel('123456789');
+//         $user->setCreatedAt(new \DateTimeImmutable());
+//         $user->setSpecialite(['Admin']);
+
+//         $this->assertSame('test@example.com', $user->getEmail());
+//         $this->assertSame(['ROLE_USER'], $user->getRoles());
+//         $this->assertSame('password123', $user->getPassword());
+//         $this->assertSame('John Doe', $user->getNom());
+//         $this->assertSame('123 Street', $user->getAdresse());
+//         $this->assertSame('123456789', $user->getTel());
+//         $this->assertInstanceOf(\DateTimeImmutable::class, $user->getCreatedAt());
+//         $this->assertSame(['Admin'], $user->getSpecialite());
+//     }
+// } 
